200a9864efc398dd2bd175f96c53f4b65ac16a60
Bryan Rodriguez <bsteven1993@gmail.com>
2019-04-24 17:24:16 -0500

    config port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Listen on port 3000');
+//Settings
+
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+
+//Routes
+
+//Static files
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
